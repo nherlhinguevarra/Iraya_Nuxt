7b49f2c4df3da76374da8915530f7061f445996a
nherlhinguevarra <guevarra.nherlhin13@gmail.com>
2025-06-09 03:51:04 +0800

    created data-digestion, data-factory, data-ingestion, knowledge-extraction, knowledge-workflows, valu-proposition pages, ongoing people page, adjusted header for new page routes, added viewport in nuxt config

--- pages/data-factory.vue
+++ pages/data-factory.vue
@@ -4,27 +4,29 @@
         <div class="absolute inset-0 bg-[url('https://demoiraya.bicoluni-busina.com/wp-content/uploads/2025/06/IRAYA-BACKGROUND.webp')] bg-cover bg-top bg-no-repeat bg-fixed z-0"></div>
 
         <div class="relative z-10 flex w-full h-full">
-            <div class="w-1/4 mt-20 pl-40 pt-[6.5rem]">
+            <div class="w-1/4 mt-20 ml-50 pt-[6.5rem]">
                 <h1 class="text-[#F47625] text-2xl mb-8 font-semibold">SERVICES</h1>
-                <h6 class="text-[#535353] text-lg font-medium mb-1 hover:text-[#000000]">Data Factory</h6>
-                <h6 class="text-[#535353] text-lg font-medium hover:text-[#000000]">Knowledge Workflows</h6>
+                <ul>
+                    <li class="text-[#535353] text-base font-medium mb-1 hover:text-[#000000]">Data Factory</li>
+                    <li class="text-[#535353] text-base font-medium mb-1 hover:text-[#000000]">Knowledge Workflows</li>
+                </ul>
             </div>
             <div class="w-3/4 mt-20 p-4">
-                <h1 class="text-[#535353] text-6xl leading-[3.75rem] mb-14">
+                <h1 class="text-[#535353] text-5xl leading-[3.75rem] mb-10 pr-40">
                     <span class="text-[#666666]">We are</span>
                     <span class="italic text-[#F47625] font-bold"> experts</span> 
                     in <span class="text-[#2D918C] font-bold">data mining</span> with experience across the world
                 </h1>
                 <h2 class="text-[#F47625] text-3xl font-bold mb-5">DATA FACTORY (DIGITALIZATION)</h2>
-                <p class="text-[#666666] text-lg font-medium pr-64">
+                <p class="text-[#666666] text-base font-normal pr-44 mb-2">
                     The data mining includes initial download, cataloging, cleanup, quality control of your data creating an 
                     advanced and functional web-based geoscience data repository including:
-                    <ul class="text-[#666666] text-lg font-medium list-disc ml-10 mb-5 pr-64">
+                    <ul class="text-[#666666] text-base font-normal list-disc ml-10 mb-5 pr-44">
                         <li>Crawl & Scrape</li>
                         <li>
                             Automated extraction of engineering and geoscience data from the web, open-source datasets, 
                             including but not limited to the following formats:
-                            <ul>
+                            <ul class="text-[#666666] text-base font-normal list-disc ml-10 pr-44">
                                 <li>LAS & DLIS</li>
                                 <li>SEGYs</li>
                                 <li>Documents, including DOCX, XLSX, PPTX, TXT</li>
@@ -40,7 +42,7 @@
                         <li>DAVE (Delete, Add, View & Edit)</li>
                     </ul>
                 </p>
-                <p class="text-[#666666] text-lg font-medium pr-64 mb-14">
+                <p class="text-[#666666] text-base font-normal pr-44 mb-14">
                     Get your images organized through image classification for efficient processing and analysis. Move from 
                     cats vs dogs classification to engineering and geoscience image classes such as maps, seismic, 
                     stratigraphic charts, cores, tables, and charts.
---
+++ pages/value-proposition.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="relative w-screen max-w-full">
+    <!-- Fixed background image -->
+        <div class="absolute inset-0 bg-[url('https://demoiraya.bicoluni-busina.com/wp-content/uploads/2025/06/IRAYA-BACKGROUND.webp')] bg-cover bg-top bg-no-repeat bg-fixed z-0"></div>
+
+        <div class="relative z-10 flex w-full h-full">
+            <div class="w-1/4 mt-20 ml-30 pt-[6.5rem]">
+                <h1 class="text-[#F47625] text-2xl mb-8 font-semibold">WHY IRAYA?</h1>
+                <ul>
+                    <li class="text-[#535353] text-base font-medium mb-1 hover:text-[#000000]">Value Proposition</li>
+                    <li class="text-[#535353] text-base font-medium mb-1 hover:text-[#000000]">Data Ingestion</li>
+                    <li class="text-[#535353] text-base font-medium mb-1 hover:text-[#000000]">Data Digestion</li>
+                    <li class="text-[#535353] text-base font-medium mb-1 hover:text-[#000000]">Knowledge Extraction</li>
+                </ul>
+            </div>
+            <div class="w-3/4 mt-20 p-4">
+                <h1 class="text-[#535353] text-5xl leading-[3.75rem] mb-3 pr-40">
+                    <span class="text-[#666666]">We make</span>
+                    <span class="text-[#F47625] font-bold"> unstructured data</span> 
+                    instantly <span class="text-[#2D918C] font-bold">available</span> 
+                    at your <span class="font-bold"> fingertips</span>
+                </h1>
+                <p class="text-[#666666] text-base font-normal pr-44 mb-6">
+                    The oil and gas industry is awash with data, both unstructured and structured, which expands 
+                    exponentially and are everywhere. Through the ElasticDocs, Intuitive Knowledge Container, decades 
+                    of engineering, exploration, and production data become instantly available.
+                    <br><br>
+                    Engineering, exploration, and production data are available in an unstructured manner including final 
+                    well reports, completion reports, drilling reports, seismic processing reports, and facility management 
+                    documents as examples of the huge and diverse range of data, disciplines, and formats. The files are 
+                    commonly in .pdf, .docx, .xlsx, pptx, .las and .segy formats. This data is ingested through a consecutive 
+                    pipeline and workflow using machine learning techniques such as natural language processing and 
+                    deep convolutional neural network to provide the user with a structured set of data. The ingested 
+                    documents become available for data analysis making it possible to extract information and new 
+                    knowledge through the ElasticDocs Intuitive Knowledge Container.
+                </p>
+                <h2 class="text-[#535353] text-2xl font-bold mb-2">
+                    <span class="text-[#2D918C] font-bold">4V's</span> in
+                    <span class="text-[#F47625] font-bold">Big Data</span>
+                </h2>
+                <p class="text-[#666666] text-base font-normal pr-44 mb-6">
+                    The ElasticDocsTM Intuitive Knowledge Container manages the 4V’s in big data: A substantial Volume 
+                    of unstructured data are organized from a large Variety of document types. All information is available 
+                    instantaneously (Velocity) and with a high degree of accuracy (Veracity).
+                </p>
+                <h2 class="text-[#535353] text-2xl font-bold mb-2">Why is
+                    <span class="text-[#2D918C] font-bold">ElasticDocs™</span> the
+                    <span class="text-[#F47625] font-bold">right solution</span> for your company?
+                </h2>
+                <ul class="text-[#666666] text-base font-normal list-disc ml-10 mb-6 pr-44">
+                    <li>Fast browsing of documents offers geoscientists new evidence-based insights</li>
+                    <li>Share digitized data across the full exploration and production value chain and have strategic 
+                        information instantly available</li>
+                    <li>Maximize the value of your digitized data by giving access to diverse, multi-location subsurface
+                        teams</li>
+                    <li>Save valuable geoscientist time in data mining and lower exploration and production project costs</li>
+                    <li>Geolocate your information to add a spatial context to new insights</li>
+                    <li>Discover unknown patterns in your geoscience documents</li>
+                    <li>Develop an exploration strategy by having decades of exploration history at your fingertips</li>
+                </ul>
+                <div class="relative inline-block mr-44">
+                    <!-- Background rectangle with shadow -->
+                    <div class="absolute inset-0 bg-white shadow-2xl rounded-lg" aria-hidden="true"></div>
+
+                    <!-- Transparent image on top -->
+                    <img
+                        src="https://demoiraya.bicoluni-busina.com/wp-content/uploads/2025/06/Infographic-01_270321-scaled.webp"
+                        alt=""
+                        class="relative z-10 w-full h-auto object-contain"
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
