6b5c6ee99626d1fa0ccb573e9e3a92e8d215edcb
ronamay-0408 <ronamay.balangat@bicol-u.edu.ph>
2025-06-17 16:14:18 +0800

    blog page added!!

--- pages/resources/blog.vue
+++ pages/resources/blog.vue
@@ -1,23 +1,227 @@
 <template>
-  <div class="p-8">
-    <h1 class="text-2xl font-bold mb-4">Blog Posts</h1>
-    <div v-if="posts?.length">
-      <div v-for="post in posts" :key="post.id" class="mb-6">
-        <h2 class="text-xl font-semibold" v-html="post.title.rendered" />
-        <div v-html="post.excerpt.rendered" class="text-gray-700" />
-      </div>
+    <div class="h-5 lg:h-15"></div>
+    <div class="px-5 text-center pt-10 pb-0 lg:pt-20 lg:pb-5 w-full lg:w-[90%] xl:w-[60%] mx-auto">
+        <h1 class="text-[#535353] text-[30px] lg:text-[40px] xl:text-[50px] font-medium">
+            Get the latest about 
+            <span class="text-[#F47625] italic">Iraya's </span>
+            products, technology, and events
+        </h1>
     </div>
+    <div>
+        <h1 class="text-center text-[#F47625] text-[40px] lg:text-[50px] xl:text-[60px] font-bold">
+            EVENTS
+        </h1>
+        <div class="w-full lg:w-[90%] xl:w-[70%] mx-auto px-5">
+            <h3 class="text-[20px] text-[#F47625] font-medium underline py-2 lg:py-10">
+                Older Entries >
+            </h3>
+            <div class="flex flex-col lg:flex-row gap-10">
+                <div class="lg:w-1/2 shadow-[0_10px_40px_rgba(0,0,0,0.3)] rounded-[20px]">
+                    <img src="/images/On-Premise-Post-940x675.png" alt="">
+                    <div class="text-center p-5 lg:p-10 leading-[1.2]">
+                        <h4 class="text-[18px] font-regular">ELASTICDOCS ON PREMISE TRAINING IN KUALA LUMPUR</h4>
+                        <p class="text-[#535353] text-[14px] py-3">Sep 5, 2024</p>
+                    </div>
+                </div>
+                <div class="lg:w-1/2 shadow-[0_10px_40px_rgba(0,0,0,0.3)] rounded-[20px]">
+                    <img src="/images/JLM-New-Energy-Nexus-2.png" alt="">
+                    <div class="text-center p-5 lg:p-10 leading-[1.2]">
+                        <h4 class="text-[18px] font-regular">IRAYA AT NEW ENERGY NEXUS PHILIPPPINES’ ENERGY CONNEX MEETUP</h4>
+                        <p class="text-[#535353] text-[14px] py-3">Sep 14, 2023</p>
+                    </div>
+                </div>
+                <div class="lg:w-1/2 shadow-[0_10px_40px_rgba(0,0,0,0.3)] rounded-[20px]">
+                    <img src="/images/KLGG-Rizal-1080x675.png" alt="">
+                    <div class="text-center p-5 lg:p-10 leading-[1.2]">
+                        <h4 class="text-[18px] font-regular">GEOSCIENCE LECTURE SERIES WITH KUALA LUMPUR GEOSCIENTISTS GROUP (KLGG)</h4>
+                        <p class="text-[#535353] text-[14px] py-3">Sep 14, 2023</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="h-5 lg:h-20"></div>
+    <div class="w-full lg:w-[90%] xl:w-[60%] mx-auto px-5">
+        <h1 class="text-center text-[#F47625] text-[40px] lg:text-[50px] xl:text-[60px] font-bold py-5 lg:py-10">
+            NEWS
+        </h1>
+        <div class="flex flex-col lg:flex-row gap-10">
+            <div class="lg:w-1/2 shadow-[0_10px_40px_rgba(0,0,0,0.3)] rounded-[20px]">
+                <img src="/images/On-Premise-Post-940x675.png" alt="">
+                <div class="p-5 lg:p-10 leading-[1.2]">
+                    <h4 class="text-[26px] font-regular">ELASTICDOCS ON PREMISE TRAINING IN KUALA LUMPUR</h4>
+                    <p class="text-[#535353] text-[14px] py-3">Sep 5, 2024 | Events, News</p>
+                    <p class="text-[#535353] text-[18px] py-3 leading-[1.5] text-justify">
+                        🚀 Get Ready to Elevate Your E&P Knowledge Management Skills and join us 
+                        for the ElasticDocs On Premise Training Course.📅 Date: 10 September 2024📍 
+                        Venue: Kuala Lumpur, Malaysia🎯 Why Attend?* Hands-on training with ElasticDocs experts* Network...
+                    </p>
+                    <h3 class="text-center text-[20px] text-[#F47625] font-medium underline py-2 lg:py-5">
+                        Read More >
+                    </h3>
+                </div>
+            </div>
 
-    <div v-else>
-      <p>Loading posts...</p>
+            <div class="lg:w-1/2 shadow-[0_10px_40px_rgba(0,0,0,0.3)] rounded-[20px]">
+                <img src="/images/JLM-New-Energy-Nexus-2.png" alt="">
+                <div class="p-5 lg:p-10 leading-[1.2]">
+                    <h4 class="text-[26px] font-regular uppercase">Iraya at New Energy Nexus Philippines’ Energy Connex Meetup</h4>
+                    <p class="text-[#535353] text-[14px] py-3">Sep 14, 2023 | Events, New Energy, News</p>
+                    <p class="text-[#535353] text-[18px] py-3 leading-[1.5] text-justify">
+                        On the 29th of July 2023, Our AI Solutions Manager, Jonathan Lee Monasterial 
+                        had the opportunity to give a talk on the New Energy Nexus Philippines' 
+                        Energy Connex Meetup with the theme 'Igniting Change: Startups Shaping the...
+                    </p>
+                    <h3 class="text-center text-[20px] text-[#F47625] font-medium underline py-2 lg:py-5">
+                        Read More >
+                    </h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="h-5 lg:h-10"></div>
+        <div class="flex flex-col lg:flex-row gap-10">
+            <div class="lg:w-1/2 shadow-[0_10px_40px_rgba(0,0,0,0.3)] rounded-[20px]">
+                <img src="/images/KLGG-Rizal-1080x675.png" alt="">
+                <div class="p-5 lg:p-10 leading-[1.2]">
+                    <h4 class="text-[26px] font-regular uppercase">Our CEO at the Agora Pod Talk in Energy Asia 2023</h4>
+                    <p class="text-[#535353] text-[14px] py-3">Sep 14, 2023 | Conference, Events, New Energy, News</p>
+                    <p class="text-[#535353] text-[18px] py-3 leading-[1.5] text-justify">
+                        📣 We are proud to announce Nina Marie Hernandez, our CEO will be a presenter at the 
+                        upcoming #EnergyAsia2023 which is collaborating with CERAWeek. 
+                        Happening from the 26th to the 28th of June 2023. ♻ Energy Asia advances the...
+                    </p>
+                    <h3 class="text-center text-[20px] text-[#F47625] font-medium underline py-2 lg:py-5">
+                        Read More >
+                    </h3>
+                </div>
+            </div>
+
+            <div class="lg:w-1/2 shadow-[0_10px_40px_rgba(0,0,0,0.3)] rounded-[20px]">
+                <img src="/images/Energy-Asia-Speaker-NMH-1080x627.png" alt="">
+                <div class="p-5 lg:p-10 leading-[1.2]">
+                    <h4 class="text-[26px] font-regular uppercase">Iraya at New Energy Nexus Philippines’ Energy Connex Meetup</h4>
+                    <p class="text-[#535353] text-[14px] py-3">Sep 14, 2023 | Events, New Energy, News</p>
+                    <p class="text-[#535353] text-[18px] py-3 leading-[1.5] text-justify">
+                        On the 29th of July 2023, Our AI Solutions Manager, Jonathan Lee Monasterial 
+                        had the opportunity to give a talk on the New Energy Nexus Philippines' 
+                        Energy Connex Meetup with the theme 'Igniting Change: Startups Shaping the...
+                    </p>
+                    <h3 class="text-center text-[20px] text-[#F47625] font-medium underline py-2 lg:py-5">
+                        Read More >
+                    </h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="h-5 lg:h-10"></div>
+        <div class="flex flex-col lg:flex-row gap-10">
+            <div class="lg:w-1/2 shadow-[0_10px_40px_rgba(0,0,0,0.3)] rounded-[20px]">
+                <img src="/images/ea2-980x738.png" alt="">
+                <div class="p-5 lg:p-10 leading-[1.2]">
+                    <h4 class="text-[26px] font-regular uppercase">Iraya at Energy Asia 2023</h4>
+                    <p class="text-[#535353] text-[14px] py-3">Sep 14, 2023 | Conference, Events, New Energy, News</p>
+                    <p class="text-[#535353] text-[18px] py-3 leading-[1.5] text-justify">
+                        Energy Asia 2023 is LIVE! 📣 Representing Iraya Energies are our CEO, 
+                        Nina Marie Hernandez, AI Solutions Manager Jonathan Lee Monasterial and Philippines' 
+                        Country Director, Anabella Sevilla. Do come and say hi if you are also...
+                    </p>
+                    <h3 class="text-center text-[20px] text-[#F47625] font-medium underline py-2 lg:py-5">
+                        Read More >
+                    </h3>
+                </div>
+            </div>
+
+            <div class="lg:w-1/2 shadow-[0_10px_40px_rgba(0,0,0,0.3)] rounded-[20px]">
+                <img src="/images/YF-SEG-830x675.png" alt="">
+                <div class="p-5 lg:p-10 leading-[1.2]">
+                    <h4 class="text-[26px] font-regular uppercase">Iraya at SEG/SPE Workshop on Reservoir Characterization</h4>
+                    <p class="text-[#535353] text-[14px] py-3">Sep 14, 2023 | Events, News</p>
+                    <p class="text-[#535353] text-[18px] py-3 leading-[1.5] text-justify">
+                        📣 ONE DAY LEFT for the much anticipated #SEG#SPE workshop on #reservoircharacterization! 
+                        👩‍💼 Representing Iraya on the 21st of June (the second day of the workshop) 
+                        will be our Chief Commercial Officer (CCO), Yang Fairuz as...
+                    </p>
+                    <h3 class="text-center text-[20px] text-[#F47625] font-medium underline py-2 lg:py-5">
+                        Read More >
+                    </h3>
+                </div>
+            </div>
+        </div>
+        
+        <div class="h-5 lg:h-10"></div>
+        <div class="flex flex-col lg:flex-row gap-10">
+            <div class="lg:w-1/2 shadow-[0_10px_40px_rgba(0,0,0,0.3)] rounded-[20px]">
+                <img src="/images/FB-French-Tech-800x675.png" alt="">
+                <div class="p-5 lg:p-10 leading-[1.2]">
+                    <h4 class="text-[26px] font-regular uppercase">A Collaborative Effort With La French Tech Malaysia</h4>
+                    <p class="text-[#535353] text-[14px] py-3">Sep 14, 2023 | Events, News</p>
+                    <p class="text-[#535353] text-[18px] py-3 leading-[1.5] text-justify">
+                        🤝 On the evening of 19th June 2023, a sharing session was held which was a 
+                        collaborative effort of Iraya Energies with La French Tech Malaysia. 
+                        The venue will be a casual coffee shop called CO2 at TTDI, so don't 
+                        forget to grab yourselves a cup of...
+                    </p>
+                    <h3 class="text-center text-[20px] text-[#F47625] font-medium underline py-2 lg:py-5">
+                        Read More >
+                    </h3>
+                </div>
+            </div>
+
+            <div class="lg:w-1/2 shadow-[0_10px_40px_rgba(0,0,0,0.3)] rounded-[20px]">
+                <img src="/images/NMH-Colombia-1065x675.png" alt="">
+                <div class="p-5 lg:p-10 leading-[1.2]">
+                    <h4 class="text-[26px] font-regular uppercase">Our Technology Partners in Colombia</h4>
+                    <p class="text-[#535353] text-[14px] py-3">Sep 14, 2023 | News</p>
+                    <p class="text-[#535353] text-[18px] py-3 leading-[1.5] text-justify">
+                        Fresh from the awesome EAGE (European Association of Geoscientists and Engineers) 
+                        (European Association of Geoscientists and Engineers) Annual in Vienna last week our CEO 
+                        Nina Marie Hernandez continued w her journey to meet in-person our equally...
+                    </p>
+                    <h3 class="text-center text-[20px] text-[#F47625] font-medium underline py-2 lg:py-5">
+                        Read More >
+                    </h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="h-5 lg:h-10"></div>
+        <div class="flex flex-col lg:flex-row gap-10">
+            <div class="lg:w-1/2 shadow-[0_10px_40px_rgba(0,0,0,0.3)] rounded-[20px]">
+                <img src="/images/MicrosoftTeams-image-8-980x738.png" alt="">
+                <div class="p-5 lg:p-10 leading-[1.2]">
+                    <h4 class="text-[26px] font-regular uppercase">Iraya Joins Luncheon Hosted by the Philippines Mining CLub</h4>
+                    <p class="text-[#535353] text-[14px] py-3">Sep 13, 2023 | Events, News</p>
+                    <p class="text-[#535353] text-[18px] py-3 leading-[1.5] text-justify">
+                        💫 Previously on the 2nd of June 2023, it was an exciting event and a delightful 
+                        #luncheon hosted by The Philippine Mining Club, as well as presenting TVI Pacific, 
+                        Inc Chairman, Mr. Clifford M. James. 🎤 Mr. James had talked about "TVI...
+                    </p>
+                    <h3 class="text-center text-[20px] text-[#F47625] font-medium underline py-2 lg:py-5">
+                        Read More >
+                    </h3>
+                </div>
+            </div>
+
+            <div class="lg:w-1/2 shadow-[0_10px_40px_rgba(0,0,0,0.3)] rounded-[20px]">
+                <img src="/images/EAGE-Speakers-Announcement-1080x675.png" alt="">
+                <div class="p-5 lg:p-10 leading-[1.2]">
+                    <h4 class="text-[26px] font-regular uppercase">Iraya Energies at the EAGE Annual 2023</h4>
+                    <p class="text-[#535353] text-[14px] py-3">Sep 13, 2023 | Conference, Events, News</p>
+                    <p class="text-[#535353] text-[18px] py-3 leading-[1.5] text-justify">
+                        ⭐ On to the next upcoming conference, the EAGE (European Association of Geoscientists 
+                        and Engineers) Annual 2023 and we are excited to share the schedule of our presentation 
+                        there! 🌆 This year, the venue will be Vienna, the capital of... 
+                    </p>
+                    <h3 class="text-center text-[20px] text-[#F47625] font-medium underline py-2 lg:py-5">
+                        Read More >
+                    </h3>
+                </div>
+            </div>
+        </div>
+
+        <h3 class="text-[20px] text-[#F47625] font-medium underline py-5 lg:py-10">
+            << Older Entries
+        </h3>
     </div>
-  </div>
 </template>
-
-<script setup>
-const { data: posts, error } = await useFetch('https://demoiraya.bicoluni-busina.com/wp-json/wp/v2/posts')
-
-if (error.value) {
-  console.error('Failed to fetch posts:', error.value)
-}
-</script>
