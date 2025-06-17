b5848b57419afff71c776002ece2502eadf53ef3
ronamay-0408 <ronamay.balangat@bicol-u.edu.ph>
2025-06-17 23:28:04 +0800

    update!

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,38 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  pages: true,
-  devtools: { enabled: true },
+    compatibilityDate: '2025-05-15',
+    pages: true,
+    devtools: { enabled: true },
 
-  runtimeConfig: {
-    public: {
-      wpApiBase: 'https://https://demoiraya.bicoluni-busina.com//wp-json' // <-- Replace with your site
-    }
-  },
-
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts'],
-
-  app: {
-    head: {
-      meta: [
-        {
-          name: 'vieport',
-          content: 'width=device-width, initial-scale=1.0',
+    runtimeConfig: {
+        public: {
+        wpApiBase: 'https://https://demoiraya.bicoluni-busina.com//wp-json' // <-- Replace with your site
         }
-      ],
-      link: [
-        { rel: 'stylesheet', 
-          href: 'https://fonts.googleapis.com/css2?family=Exo+2:wght@100..900&family=Quicksand:wght@300..700&display=swap'
-        },
-        {
-          rel: 'stylesheet',
-          href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css'
-        },
-        {
-          rel: 'stylesheet',
-          href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
+    },
+
+    modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts'],
+
+    app: {
+        head: {
+            meta: [
+                {
+                name: 'vieport',
+                content: 'width=device-width, initial-scale=1.0',
+                }
+            ],
+            link: [
+                { 
+                    rel: 'stylesheet', 
+                    href: 'https://fonts.googleapis.com/css2?family=Exo+2:wght@100..900&family=Quicksand:wght@300..700&display=swap'
+                },
+                {
+                    rel: 'stylesheet',
+                    href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css'
+                },
+                {
+                    rel: 'stylesheet',
+                    href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
+                }
+            ]
         }
-      ]
     }
-  }
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,28 @@
 // tailwind.config.js
 module.exports = {
-  content: [
-    './components/**/*.{vue,js,ts}',
-    './layouts/**/*.{vue,js,ts}',
-    './pages/**/*.{vue,js,ts}',
-    './app.vue',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        exo2: ['"Exo 2"', 'sans-serif'],
-      },
-      colors: {
-        orange: {
-          500: '#F47625',
+    content: [
+        './components/**/*.{vue,js,ts}',
+        './layouts/**/*.{vue,js,ts}',
+        './pages/**/*.{vue,js,ts}',
+        './app.vue',
+    ],
+    theme: {
+        extend: {
+        fontFamily: {
+            exo2: ['"Exo 2"', 'sans-serif'],
         },
-        bg: {
-          500: '#2D918C',
+        colors: {
+            orange: {
+            500: '#F47625',
+            },
+            bg: {
+            500: '#2D918C',
+            },
+            lightgray: {
+            200: '#666666'
+            }
         },
-        lightgray: {
-          200: '#666666'
-        }
-      },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
